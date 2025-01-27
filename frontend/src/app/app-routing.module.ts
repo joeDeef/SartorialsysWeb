ba602c3ed41a7478dec614c9dfc80b54f4989130
Alex <alex.escobar@epn.edu.ec>
2025-01-26 23:22:39 -0500

    Creacion de ruterlink y enlace con los componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
-  {path:'users',component:HomeComponent},
-  {path:'users/:id',component:HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Ruta raíz
+  { path: 'home', component: HomeComponent }, // Página de inicio
+  { path: 'users', component: HomeComponent }, // Gestión de usuarios
+  { path: 'users/:id', component: HomeComponent }, // Detalles de un usuario
+  { path: 'products/:category/:id', component: ProductDetailComponent }, // Detalles de un producto
+  { path: 'products/:category', component: ProductsComponent }, // Productos por categoría
+  { path: 'products', component: ProductsComponent }, // Todos los productos
+  { path: '**', redirectTo: 'home' }, // Ruta comodín
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductsComponent } from './products/products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ProductDetailComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
