c4e23e4bb1d6163acef640366422568ed1ac5edd
Joel Defaz <joe.def2019@gmail.com>
2025-01-29 13:52:44 -0500

    Administración: Conexion Admnistración y el Login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,41 +1,39 @@
-import { createComponent, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CreateProductComponent } from './components/create-product/create-product.component';
-import { EditarProductComponent } from './components/editar-product/editar-product.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AdministationComponent } from './components/administation/administation.component';
 import { EditProductComponent } from './components/edit-product/edit-product.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Ruta raíz
-  { path: 'home', component: HomeComponent }, // Página de inicio
-  { path: 'users', component: HomeComponent }, // Gestión de usuarios
-  { path: 'users/:id', component: HomeComponent }, // Detalles de un usuario
-  { path: 'products/:category/:id', component: ProductDetailComponent }, // Detalles de un producto
-  { path: 'products/:category', component: ProductsComponent }, // Productos por categoría
-  { path: 'products', component: ProductsComponent }, // Todos los productos
-  { path: 'create-product', component: CreateProductComponent },
-  { path: 'edit-product/:id', component: EditarProductComponent },
-  { path: 'login', component: LoginComponent},
-  { path: 'signup', component: SignupComponent},
-  { path: '**', redirectTo: 'home' }, // Ruta comodín
+  // Default route
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  // General routes
   { path: 'home', component: HomeComponent },
   { path: 'users', component: HomeComponent },
   { path: 'users/:id', component: HomeComponent },
   { path: 'products/:category/:id', component: ProductDetailComponent },
   { path: 'products/:category', component: ProductsComponent },
   { path: 'products', component: ProductsComponent },
+
+  // Authentication routes
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+
+  // Product administration routes
+  { path: 'administration', component: AdministationComponent },
+  { path: 'administration/products', component: ProductsComponent },
   { path: 'administration/products/edit-product/:id', component: EditProductComponent },
   { path: 'administration/products/create-product', component: CreateProductComponent },
   { path: 'administration/products/:id', component: ProductDetailComponent },
-  { path: 'administration', component: AdministationComponent },
-  { path: 'administration/products', component: ProductsComponent },
-  { path: '**', redirectTo: 'home' },
+
+  // Catch-all route (same as default)
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Components
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductsComponent } from './products/products.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
 import { CreateProductComponent } from './components/create-product/create-product.component';
-import { EditarProductComponent } from './components/editar-product/editar-product.component';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component'
+import { SignupComponent } from './components/signup/signup.component';
 import { AdministationComponent } from './components/administation/administation.component';
-import { EditProductComponent } from './components/edit-product/edit-product.component'
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+
+// Modules
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,9 +28,8 @@ import { EditProductComponent } from './components/edit-product/edit-product.com
     ProductDetailComponent,
     ProductsComponent,
     CreateProductComponent,
-    EditarProductComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
     AdministationComponent,
     EditProductComponent
   ],
