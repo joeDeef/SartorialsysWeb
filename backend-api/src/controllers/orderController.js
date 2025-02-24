921a0c3740cb434368ff81f489bebd62743fa667
Joel Defaz <joe.def2019@gmail.com>
2025-02-24 15:37:56 -0500

    Doc: Update Documentation and Add Errors

--- backend-api/src/controllers/orderController.js
+++ backend-api/src/controllers/orderController.js
@@ -4,6 +4,12 @@ import {
   sendErrorResponse,
   sendSuccessResponse,
 } from "../utils/messages.js";
+import {
+  CartNotFoundError,
+  SizeNotFoundError,
+  ColorNotFoundError,
+} from "../errors/cartErrors.js";
+
 
 export const saveOrder = async (req, res) => {
   try {
@@ -16,6 +22,15 @@ export const saveOrder = async (req, res) => {
     if (!order) throw Error("Errror to create order");
     sendSuccessResponse(res, "Order placed successfully", order, 201);
   } catch (error) {
+    const handledErrors = [
+      CartNotFoundError,
+      SizeNotFoundError,
+      ColorNotFoundError
+    ];
+
+    if (handledErrors.some((err) => error instanceof err)) {
+      return sendErrorResponse(res, error.message, error.statusCode);
+    }
     logsError(error);
     sendErrorResponse(res);
   }
